aaa906ff8d3e7a8c0a2eb3049761ca4c23eeaee9
Felipe <felipemachadocordeiro@gmail.com>
2022-06-01 18:49:41 -0300

    Criar projetos de infraestrutura e Aplicação

---
+++ API/Infra.Database/Context/TextBookAnswersContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+
+namespace Infra.Database.Context
+{
+    public class TextBookAnswersContext : DbContext
+    {
+        public TextBookAnswersContext(DbContextOptions<TextBookAnswersContext> options) : base(options) { }
+
+        public DbSet<Book> books;
+    }
+}
---
+++ API/Infra.Database/Repositories/BookRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Adapters;
+using Domain.Entities;
+using Infra.Database.Context;
+
+namespace Infra.Database.Repositories
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly TextBookAnswersContext textBookAnswersContext;
+
+        public BookRepository(TextBookAnswersContext textBookAnswersContext)
+        {
+            this.textBookAnswersContext = textBookAnswersContext;
+        }
+        public void Add(Book book)
+        {
+            textBookAnswersContext.books.Add(book);
+        }
+
+        public Book? Get(int id)
+        {
+            return textBookAnswersContext.books.Find(id);
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return textBookAnswersContext.books.AsEnumerable();
+        }
+    }
+}
