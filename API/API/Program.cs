dcaddea611a93b7eda4e8c976eac706a718a186e
Felipe <felipemachadocordeiro@gmail.com>
2022-06-01 19:38:12 -0300

    Criar endpoints de livros

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using Infra.Database;
+using Microsoft.AspNetCore.Http.Json;
+using Aplication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +14,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDatabaseConfiguration(builder.Configuration.GetConnectionString("DefaultConnection"));
+builder.Services.AddApplicationDependencies();
+
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+});
+
 var app = builder.Build();
+
+app.Services.SeedDatabase();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
