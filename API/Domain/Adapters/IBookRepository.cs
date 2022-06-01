445c812d65d04ee842f6c972de9d7c99ee90a3c9
Felipe <felipemachadocordeiro@gmail.com>
2022-06-01 18:46:17 -0300

    Adicionar classes de dominio dos livros

---
+++ API/Domain/Adapters/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Adapters
+{
+    public interface IBookRepository
+    {
+        IEnumerable<Book> GetAll();
+        Book? Get(int id);
+        void Add(Book book);
+    }
+}
---
+++ API/Domain/Services/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+        Book? Get(int id);
+        void Add(Book book);
+
+    }
+}
