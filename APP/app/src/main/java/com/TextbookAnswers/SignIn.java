ac4f4b95e45cac584264490a8ea27e3103864c1b
Gean <geangs1607@gmail.com>
2022-06-04 18:27:25 -0300

    tela cadastro

--- APP/app/src/main/java/com/TextbookAnswers/SignIn.java
+++ APP/app/src/main/java/com/TextbookAnswers/SignIn.java
@@ -6,21 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Login extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_in);
         this.getSupportActionBar().hide();
-
-    }
-
-    public void entrar(View view){
-        startActivity(new Intent(this,MainActivity.class));
     }
 
     public void cadastrar(View view){
-        startActivity(new Intent(this,SignIn.class));
+
+        startActivity(new Intent(this,MainActivity.class));
     }
 }
+
