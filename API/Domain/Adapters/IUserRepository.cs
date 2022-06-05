376d739da6dfaa1beb48b7ea305a9fe928e54918
Felipe <felipemachadocordeiro@gmail.com>
2022-06-05 09:31:53 -0300

    Adicionar classes de usuários no Domain

---
+++ API/Domain/Adapters/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Adapters
+{
+    public interface IUserRepository
+    {
+        void Add(User user);
+        bool Authenticate(string login, string password);
+        User? GetUser(int id);
+        User? GetUserByLogin(string login);
+    }
+}
