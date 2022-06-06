bcf0cdc9b04345a886ee9f0b93c4c404e0b51937
Felipe <felipemachadocordeiro@gmail.com>
2022-06-06 09:15:11 -0300

    Adicionar Controllers para Capítulo e pergunta

---
+++ API/API/Controllers/ChapterController.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Adapters;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ChapterController : ControllerBase
+    {
+        private readonly IChapterRepository chapterRepository;
+
+        public ChapterController(IChapterRepository chapterRepository)
+        {
+            this.chapterRepository = chapterRepository;
+        }
+
+        [HttpGet("{bookId:int}")]
+        public IEnumerable<Chapter> GetBookChapters(int bookId)
+        {
+            return chapterRepository.GetBookChapters(bookId);
+        }
+
+        [HttpPost]
+        public void AddChapter(Chapter chapter)
+        {
+            if (String.IsNullOrEmpty(chapter.Name) || chapter.BookId == 0)
+            {
+                Response.StatusCode = 400;
+                return;
+            }
+            chapterRepository.AddChapter(chapter);
+        }
+    }
+}
---
+++ API/API/Controllers/QuestionController.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Adapters;
+using Domain.Entities;
+using Infra.Database.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        private readonly IQuestionRepository questionRepository;
+
+        public QuestionController(IQuestionRepository questionRepository)
+        {
+            this.questionRepository = questionRepository;
+        }
+
+        [HttpGet("{chapterId:int}")]
+        public IEnumerable<Question> GetChapterQuestions(int chapterId)
+        {
+            return questionRepository.GetChapterQuestions(chapterId);
+        }
+
+        [HttpPost]
+        public void AddChapter(Question question)
+        {
+            if (String.IsNullOrEmpty(question.Text) || question.ChapterId == 0)
+            {
+                Response.StatusCode = 400;
+            return;
+            }
+            questionRepository.AddQuestion(question);
+        }
+    }
+}
