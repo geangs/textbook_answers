f59b709d6454ab53e54f9f34dc7bce8952a6a010
Felipe <felipemachadocordeiro@gmail.com>
2022-06-08 01:52:32 +0000

    Adicionar Autor em livros

--- API/API/Program.cs
+++ API/API/Program.cs
@@ -26,11 +26,8 @@ var app = builder.Build();
 app.Services.SeedDatabase();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
