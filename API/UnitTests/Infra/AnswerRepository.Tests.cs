3a158e47f761b5a7e785decfac60456eac651e70
Felipe <felipemachadocordeiro@gmail.com>
2022-07-02 15:11:17 -0300

    Criar testes iniciais

---
+++ API/UnitTests/Infra/AnswerRepository.Tests.cs
@@ -0,0 +1,110 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+using Infra.Database.Context;
+using Infra.Database.Repositories;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests.Infra
+{
+    class AnswerRepositoryTest
+    {
+
+        [Test]
+        public void AnswerRepository_UpvoteAnswer_CallSaveChanges()
+        {
+            var answer = new Answer()
+            {
+                Id = 1,
+                Text = "Teste",
+                Upvotes = 0,
+                Downvotes = 0,
+            };
+
+            var contextMock = new Mock<TextBookAnswersContext>();
+            contextMock.Setup(x => x.Answers.Find(It.IsAny<int>())).Returns(answer);
+
+
+            var answerRepository = new AnswerRepository(contextMock.Object);
+
+
+
+            answerRepository.UpvoteAnswer(1);
+
+            contextMock.Verify(context => context.SaveChanges(), Times.Once());
+
+        }
+
+        [Test]
+        public void AnswerRepository_UpvoteAnswer_ChangesScore()
+        {
+            var answer = new Answer()
+            {
+                Id = 1,
+                Text = "Teste",
+                Upvotes = 0,
+                Downvotes = 0,
+            };
+
+            var contextMock = new Mock<TextBookAnswersContext>();
+            contextMock.Setup(x => x.Answers.Find(It.IsAny<int>())).Returns(answer);
+
+
+            var answerRepository = new AnswerRepository(contextMock.Object);
+
+
+
+            answerRepository.UpvoteAnswer(1);
+
+            answer.Score.Should().Be(1);
+        }
+
+        [Test]
+        public void AnswerRepository_DownvoteAnswer_ChangesScore()
+        {
+            var answer = new Answer()
+            {
+                Id = 1,
+                Text = "Teste",
+                Upvotes = 0,
+                Downvotes = 0,
+            };
+
+            var contextMock = new Mock<TextBookAnswersContext>();
+            contextMock.Setup(x => x.Answers.Find(It.IsAny<int>())).Returns(answer);
+
+
+            var answerRepository = new AnswerRepository(contextMock.Object);
+
+
+
+            answerRepository.DownvoteAnswer(1);
+
+            answer.Score.Should().Be(-1);
+        }
+        //AddAnswer
+
+        [Test]
+        public void AnswerRepository_AddAnswer_SavesChanges()
+        {
+            var answer = new Answer()
+            {
+                Text = "Teste",
+                Upvotes = 0,
+                Downvotes = 0,
+            };
+
+            var contextMock = new Mock<TextBookAnswersContext>();
+            contextMock.Setup(x => x.Answers.Add(It.IsAny<Answer>()));
+
+            var answerRepository = new AnswerRepository(contextMock.Object);
+
+
+
+            answerRepository.AddAnswer(answer);
+
+            contextMock.Verify(context => context.Answers.Add(It.IsAny<Answer>()), Times.Once());
+            contextMock.Verify(context => context.SaveChanges(), Times.Once());
+        }
+    }
+}
---
+++ API/UnitTests/Infra/UserRepository.Test.cs
@@ -0,0 +1,148 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+using Infra.Database.Context;
+using Infra.Database.Repositories;
+using Moq;
+using Moq.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace UnitTests.Infra
+{
+    public class UserRepositoryTeste
+    {
+        [Test]
+        public void UserRepository_AddUser_SavesChanges()
+        {
+            var user = new User()
+            {
+                Name = "Teste",
+                Email = "Teste",
+                Login = "Teste",
+                Password = "Teste"
+            };
+
+            var contextMock = new Mock<TextBookAnswersContext>();
+            contextMock.Setup(x => x.Users.Add(It.IsAny<User>()));
+
+            var userRepository = new UserRepository(contextMock.Object);
+
+
+
+            userRepository.Add(user);
+
+            contextMock.Verify(context => context.Users.Add(It.IsAny<User>()), Times.Once());
+            contextMock.Verify(context => context.SaveChanges(), Times.Once());
+        }
+
+        [Test]
+        public void UserRepository_AddUser_HashesPassword()
+        {
+            var user = new User()
+            {
+                Name = "Teste",
+                Email = "Teste",
+                Login = "Teste",
+                Password = "Teste"
+            };
+
+            var contextMock = new Mock<TextBookAnswersContext>();
+            contextMock.Setup(x => x.Users.Add(It.IsAny<User>()));
+
+            var userRepository = new UserRepository(contextMock.Object);
+
+
+
+            userRepository.Add(user);
+            user.Password.Should().NotBe("Teste");
+        }
+
+        [Test]
+        public void UserRepository_AddUser_HashedPassowrd_ShouldContainSalt()
+        {
+            var user = new User()
+            {
+                Name = "Teste",
+                Email = "Teste",
+                Login = "Teste",
+                Password = "Teste"
+            };
+
+            var contextMock = new Mock<TextBookAnswersContext>();
+            contextMock.Setup(x => x.Users.Add(It.IsAny<User>()));
+
+            var userRepository = new UserRepository(contextMock.Object);
+
+
+
+            userRepository.Add(user);
+            user.Password.Should().Contain(".");
+        }
+
+        [Test]
+        public void UserRepository_Authenticate_SuccessfulyAuthenticate()
+        {
+            var user = new User()
+            {
+                Name = "Teste",
+                Email = "Teste",
+                Login = "Teste",
+                Password = "zrH8FyfhqmcnXdHBkOBY7w==.ik6GzApeYcdgeRMx7ExLLpnQB7+o6WRQzw2ujlwTCJk=" //Hashed password "Teste"
+            };
+
+            var contextMock = new Mock<TextBookAnswersContext>();
+            contextMock.Setup(x => x.Users).ReturnsDbSet(new List<User>() { user});
+
+            var userRepository = new UserRepository(contextMock.Object);
+
+
+
+            var response = userRepository.Authenticate("Teste","Teste");
+            response.Should().BeTrue();
+        }
+
+        [Test]
+        public void UserRepository_Authenticate_WrongPassword_FailsAuthentication()
+        {
+            var user = new User()
+            {
+                Name = "Teste",
+                Email = "Teste",
+                Login = "Teste",
+                Password = "zrH8FyfhqmcnXdHBkOBY7w==.ik6GzApeYcdgeRMx7ExLLpnQB7+o6WRQzw2ujlwTCJk=" //Hashed password "Teste"
+            };
+
+            var contextMock = new Mock<TextBookAnswersContext>();
+            contextMock.Setup(x => x.Users).ReturnsDbSet(new List<User>() { user });
+
+            var userRepository = new UserRepository(contextMock.Object);
+
+
+
+            var response = userRepository.Authenticate("Teste", "asdasdasdasd");
+            response.Should().BeFalse();
+        }
+
+        [Test]
+        public void UserRepository_Authenticate_UserNotExists_FailsAuthentication()
+        {
+            var user = new User()
+            {
+                Name = "Teste",
+                Email = "Teste",
+                Login = "Teste",
+                Password = "zrH8FyfhqmcnXdHBkOBY7w==.ik6GzApeYcdgeRMx7ExLLpnQB7+o6WRQzw2ujlwTCJk=" //Hashed password "Teste"
+            };
+
+            var contextMock = new Mock<TextBookAnswersContext>();
+            contextMock.Setup(x => x.Users).ReturnsDbSet(new List<User>() { user });
+
+            var userRepository = new UserRepository(contextMock.Object);
+
+
+
+            var response = userRepository.Authenticate("asdasdasdasdaasd", "Teste");
+            response.Should().BeFalse();
+        }
+    }
+}
